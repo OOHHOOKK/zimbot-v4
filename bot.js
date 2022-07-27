81d81994dac67fce2345ee3585ff2dea2216e2a5
OH.OK <79559728+OOHHOOKK@users.noreply.github.com>
2022-07-27 19:33:59 +0700

    .

--- bot.js
+++ bot.js
@@ -33,10 +33,10 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
+global.owner = ['12023171579',''] //owner number +27634090203
+global.wame = ['12023171579'] 
+global.pemilik = ['12023171579'] //owner number +27634090203
+global.premium = ['12023171579'] //premium number +27634090203
 global.pengguna = 'Drips' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
